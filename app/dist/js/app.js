a0e32a0ff0d98633b1d3f52fb3a7f6f0d5da8a39
Столповских Михаил <monkeysbrain@yandex.ru>
2017-05-15 16:09:43 +0500

    compete desktop

--- app/dist/js/app.js
+++ app/dist/js/app.js
@@ -87,12 +87,65 @@ $(document).ready(function(){
   });
 });
 
-if($(window).width() < 2767) {
-    console.log("min")
-    $(".blocks").addClass("genre owl-carousel")
-} else {
-    $(".blocks").removeClass("genre owl-carousel")
-}
 
+if($(window).width() < 767) {
+    console.log("min")
+    $(".blocks").addClass("genre owl-carousel");
+    $(".genres").addClass("genres__slider owl-carousel");
+} else {
+    $(".blocks").removeClass("genre owl-carousel");
+    $(".genres").removeClass("genres__slider owl-carousel");
+}
 });
 
+$(function(){
+  $('.blocks .item').click(function(){
+    $('.blocks .item').removeClass('item__current');
+    $(this).addClass('item__current');
+  });
+  $('.genres .item').click(function(){
+    $(this).toggleClass('item__current');
+  });
+});
+
+$(function(){
+  $('.js__show').click(function(){
+    $(this).parent().toggleClass('js__show--all');
+  });
+});
+
+
+$(document).ready(function(){
+  $('span.info').click(function(){
+    $('.news-list--card').hide(500)
+    $(this).parent('.serial__item__news').children('.news-list--card').show(500);
+  });
+});
+
+/*
+$(document).ready(function(){
+  $('.literal__item').hover(function(){
+    $(this).children('.literal__item--card').fadeToggle(500);
+  });
+});
+*/
+
+$(document).ready(function(){
+  $(".genres__slider").owlCarousel({
+    loop:false,
+    stagePadding: 100,
+    nav:false,
+    margin:10,
+    responsive:{
+      0:{
+        items:3
+      },
+      600:{
+        items:5
+      },
+      1000:{
+        items:7
+      }
+    }
+  });
+});
